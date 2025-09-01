fb6a0230237c7ef83568ee08b00eb00acb6add71
Elisapv <mpv@uc.cl>
2025-08-31 21:41:25 -0400

    actualizar

--- entrega.py
+++ entrega.py
@@ -6,7 +6,7 @@ data = {
     "rut": "204443270",
     "nombre": "Elisa",
     "apellido": "Peña",
-    "url": "https://tu-app-deploy.com",
+    "url": "https://dashboard-licitaciones-gjcdfphhzb2aimxapumfjz.streamlit.app/",
     "repo": "https://github.com/Elisapv/dashboard-licitaciones.git",
     "comentario": "Soy desarrollador fullstack y completé el desafío implementando un mini sistema de licitaciones con Streamlit y Supabase."
 }
