d5bae36ec5b7778b408c35b35c002953fb2541c6
Elisapv <mpv@uc.cl>
2025-08-31 21:26:28 -0400

    actualizar

---
+++ entrega.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://kaiken.up.railway.app/webhook/applicant"
+
+data = {
+    "rut": "204443270",
+    "nombre": "Elisa",
+    "apellido": "Peña",
+    "url": "https://tu-app-deploy.com",
+    "repo": "https://github.com/Elisapv/dashboard-licitaciones.git",
+    "comentario": "Soy desarrollador fullstack y completé el desafío implementando un mini sistema de licitaciones con Streamlit y Supabase."
+}
+
+response = requests.post(url, json=data)
+
+print(response.status_code)
+print(response.text)
